719701d97785be114b8118fa0e30f98a435de4e2
Harpinder Singh <harpinder.singh@rvsolutions.in>
2024-12-09 12:17:46 +0530

    Added the new font

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ st.markdown("""
     </style>
     <div class="video-background-container">
         <video autoplay loop muted playsinline preload="auto">
-            <source src="https://github.com/MrSingh529/your-dashboard/blob/8e8e36efa39a3ebbb23c4de9d3efe8710ab2c542/assets/bg.mp4?raw=true" type="video/mp4">
+            <source src="https://github.com/MrSingh529/your-dashboard/blob/af4e0a2ef4772ff86936c549948468445affc882/assets/bg.mp4?raw=true" type="video/mp4">
             Your browser does not support the video tag.
         </video>
     </div>
